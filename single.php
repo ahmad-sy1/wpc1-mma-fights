0f34482e5a9a4d383bda1616d948159e8cf16fd6
Ahmad Alasmi <ahas.alasmi@gmail.com>
2026-09-02 10:43:37 +0200

    Stap 2: single.php toegevoegd voor het tonen van één bericht
    - Toont titel als h1, publicatiedatum, auteur en volledige content via the_content()
    - Categorieen van het bericht en navigatie naar vorige/volgende bericht

---
+++ single.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * single.php - toont één enkel bericht op een eigen pagina.
+ * WordPress kiest dit bestand automatisch bij het openen van een bericht.
+ */
+
+get_header();
+?>
+
+<?php
+// The Loop: bij een enkel bericht zit er precies één item in de lus.
+if ( have_posts() ) :
+    while ( have_posts() ) :
+        the_post();
+        ?>
+
+        <article <?php post_class(); ?>>
+            <h1><?php the_title(); ?></h1>
+
+            <p class="post-meta">
+                Geplaatst op <?php the_time( 'j F Y' ); ?> door <?php the_author(); ?>
+            </p>
+
+            <?php // Volledige inhoud van het bericht ?>
+            <div class="post-content"><?php the_content(); ?></div>
+
+            <p class="post-categories">Categorie: <?php the_category( ', ' ); ?></p>
+        </article>
+
+        <?php // Links naar vorige/volgende bericht ?>
+        <?php the_post_navigation(); ?>
+
+        <?php
+    endwhile;
+endif;
+?>
+
+<?php
+get_footer();
